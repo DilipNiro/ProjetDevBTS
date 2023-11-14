8bfc73f07f18dd11bd2520ba572ac754652b9e2d
niro <nirodilip.dev@gmail.com>
2023-11-14 10:48:48 +0100

    change

--- TP9/tp9.js
+++ TP9/tp9.js
@@ -8,3 +8,50 @@ function seconnecter() {
         alert("Accès refusé");
     }
 }
+
+function affichetab() {
+    document.write('<table border=2px width=30%>');
+    for (var i = 0; i <= 5; i++) {
+        document.write('<tr> <td>*</td> <td>*</td> <td>*</td> </tr>');
+    }
+    document.write('</table>');
+}
+
+function affichetab2() {
+    var asknumber = prompt("Combien de lignes souhaitez vous");
+    document.write('<table border=2px width=30%>');
+    for (var i = 0; i <= asknumber; i++) {
+        document.write("<tr> <td>" + i + "</td> <td>*</td> <td>*</td> </tr>");
+    }
+    document.write('</table>');
+}
+
+function cdc() {
+    var chaine = prompt("Donnez un mot");
+    document.write(chaine.toUpperCase() + "<br>");
+    document.write(chaine.toLocaleLowerCase() + "<br>");
+    document.write("la chaine contient " + chaine.length + " caractères" + "<br>");
+    document.write("la première lettre est " + chaine.substr(0, 1) + "<br>");
+
+}
+
+function swich() {
+    var color = prompt("Entrez une couleur");
+    switch (color) {
+        case "rouge": document.body.style.background = "red";
+            break;
+        case "ROUGE": document.body.style.background = "red";
+            break;
+        case "vert": document.body.style.background = "green";
+            break;
+        case "Vert": document.body.style.background = "green";
+            break;
+        case "rose": document.body.style.background = "pink";
+            break;
+        case "ROSE": document.body.style.background = "pink";
+            break;
+
+        default: alert("Couleur non définie");
+
+    }
+}
